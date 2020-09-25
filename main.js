ae1b85960efca3e99ef7e5e64dc9a71b2faaf92a
kshivam117@gmail.com <shivam@1994>
2020-09-25 15:45:14 +0530

    web detailing is running ok

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 //  DIsabLING INSPECT ELEMENT
 $(document).bind("contextmenu",function(e) {
    e.preventDefault();
- });
+});
 
 //  DISABLING  F12  FUNCCTION
 // $(document).keydown(function(e){
@@ -28,13 +28,12 @@ rootController.controller("MyCntrl", ['$scope', '$http', '$location', '$window',
     MeetingId = MeetingId == undefined || MeetingId.length<10 ? "" : MeetingId;
 
     var mVsualAdsArray = [];  
+    var itemsFilesOfBrand =[];
+    var itemsNamesOfBrand =[];
+            
     var mActiveBrandId = 0;
-    var mActiveItemId = 0;     
-    var mActiveItemIdSRN = 0; 
-    var mActiveItemIdIndex = 0; 
+    var mActiveItemIndex = 0; 
     
-    var itemsIdsOfBrand =[];
-    var itemsSRNOfBrand =[];
     var mChannelName = "CBO-E-DETAILING" ;
     var mRootChannelName = "CBO-E-DETAILING" ;
     var mMeethingDetail = "MEETINGS";
@@ -112,24 +111,28 @@ rootController.controller("MyCntrl", ['$scope', '$http', '$location', '$window',
         mMeetingDbRfrence.on('value', function(snapshot) {
             mMeetingDbSnapShot = snapshot.val();
             mActiveItemIdSRN = snapshot.val()["ACTIVE_ITEM_SRN"] == undefined ? "1": snapshot.val()["ACTIVE_ITEM_SRN"] ;
-            mActiveItemId = snapshot.val()["ACTIVE_ITEM_ID"];
             mActiveBrandId = snapshot.val()["ACTIVE_BRAND_ID"];
-            diplayImage(mActiveBrandId,mActiveItemId,mActiveItemIdSRN);
+            // mActiveItemId = snapshot.val()["ACTIVE_ITEM_ID"];
+            diplayImage(mActiveBrandId,mActiveItemIdSRN);
             
         });
         
-        firebase.database().ref(mMeetingChannelName+"/ITEMS").on('value', function(snapshot) {
+        firebase.database().ref(mMeetingChannelName+"/ITEMS").orderByChild("ISSELECTED").on('value', function(snapshot) {
     
             hideLoader();
+            //snapshot.sortBy("ISSELECTED","asc");
+
+            
 
             snapshot.forEach(function(childSnapshot) {
                 var childKey = childSnapshot.key;
                 var childData = childSnapshot.val();
-
+                console.log(childKey);
                 mVsualAdsArray.push(childData);
             });
+            mVsualAdsArray.reverse();
 
-            displayBrandWiseItems(mVsualAdsArray,mActiveBrandId,mActiveItemId,mActiveItemIdSRN);
+            displayBrandWiseItems(mVsualAdsArray,mActiveBrandId,mActiveItemIdSRN);
             
         });
     }
@@ -147,7 +150,7 @@ rootController.controller("MyCntrl", ['$scope', '$http', '$location', '$window',
 
     }
 
-    function displayBrandWiseItems(mVsualAdsArray,brandId,itemId,itemSRN){
+    function displayBrandWiseItems(mVsualAdsArray,brandId,itemSRN){
 
             var horizontalRow = document.getElementById("horizontalRow");
             removeAllChildNodes(horizontalRow);
@@ -175,7 +178,7 @@ rootController.controller("MyCntrl", ['$scope', '$http', '$location', '$window',
 
                         }else{
 
-                            if(mVsualAdsArray[x]["ISSELECTED"].includes("1")){
+                            if(mVsualAdsArray[x]["ISSELECTED"]){
 
                                 eachItem.className = "success-selected horizontalItem";
     
@@ -195,10 +198,9 @@ rootController.controller("MyCntrl", ['$scope', '$http', '$location', '$window',
 
                                     horizontalRow.children[t].className = "success-active horizontalItem";
 
-
                                 }else {
 
-                                    if(mVsualAdsArray[t]["ISSELECTED"].includes("1")){
+                                    if(mVsualAdsArray[t]["ISSELECTED"]){
 
                                         horizontalRow.children[t].className = "success-selected horizontalItem";
         
@@ -217,26 +219,22 @@ rootController.controller("MyCntrl", ['$scope', '$http', '$location', '$window',
 
                 }
 
-                diplayImage(brandId,itemId,itemSRN);
+                diplayImage(brandId,itemSRN);
     }
 
-    function diplayImage(brandIdStr,activeItemIdStr,activeItemSRNStr){
+    function diplayImage(brandIdStr,activeItemSRNStr){
 
         var NextBTN = document.getElementById("NextBTN");
         var PreviousBTN = document.getElementById("PreviousBTN");
 
         if(mVsualAdsArray.length>0){
 
-
             var brandId = parseInt(brandIdStr);
-            var itemId  = parseInt(activeItemIdStr);
 
             var brands = mVsualAdsArray.filter((x) => { return parseInt(x["BRAND_ID"])==brandId; });
 
             var brandObject;
             var titleString ="";
-            var itemsFilesOfBrand =[];
-            var itemsNamesOfBrand =[];
             
             //var activeItemId=0;
             var activeImageURL ="";
@@ -246,9 +244,9 @@ rootController.controller("MyCntrl", ['$scope', '$http', '$location', '$window',
                 brandObject = brands[0];
                 itemsFilesOfBrand = brandObject["FILE_NAME"].split(",");
                 itemsNamesOfBrand = brandObject["ITEM_NAME"].split(",");
-                itemsIdsOfBrand = brandObject["ITEM_ID"].split(",");
-                itemsSRNOfBrand = brandObject["SRNO"].split(",")
-
+                
+                mActiveItemIndex  =  parseInt(activeItemSRNStr);
+                
                 //var itemsLST = itemsIdsOfBrand.filter((y) => { return parseInt(itemsIdsOfBrand[y])==itemId;});
                 //activeItemId = singleItem[""];
 
@@ -259,7 +257,7 @@ rootController.controller("MyCntrl", ['$scope', '$http', '$location', '$window',
                 //         if( parseInt(itemsSRNOfBrand[y]) == parseInt(activeItemSRNStr) ){
 
                 //             mActiveItemId = itemId;
-                //             mActiveItemIdIndex = y;
+                //             mActiveItemIndex = y;
                 //             break;
                 //         }
                 //     }
@@ -267,25 +265,23 @@ rootController.controller("MyCntrl", ['$scope', '$http', '$location', '$window',
                 // }
 
 
-                mActiveItemIdIndex  =  parseInt(activeItemSRNStr);
-                mActiveItemId = itemId;
-        
+               
+    
+                // titleString =  "Visual Ad => "+(itemsIdsOfBrand[mActiveItemIndex])+" of "+ itemsIdsOfBrand.length+"<br> "
+                // + "BRAND ID : "+brandIdStr +" & ITEM NAME : "+ itemsNamesOfBrand[mActiveItemIndex];
 
-                // titleString =  "Visual Ad => "+(itemsIdsOfBrand[mActiveItemIdIndex])+" of "+ itemsIdsOfBrand.length+"<br> "
-                // + "BRAND ID : "+brandIdStr +" & ITEM NAME : "+ itemsNamesOfBrand[mActiveItemIdIndex];
-
-                // titleString =  brandObject["SRNO"]+">>"+itemsNamesOfBrand[mActiveItemIdIndex];
+                // titleString =  brandObject["SRNO"]+">>"+itemsNamesOfBrand[mActiveItemIndex];
                 
-                titleString =  itemsNamesOfBrand[mActiveItemIdIndex];
+                titleString =  itemsNamesOfBrand[mActiveItemIndex];
 
-                activeImageURL = itemsFilesOfBrand[mActiveItemIdIndex];
+                activeImageURL = itemsFilesOfBrand[mActiveItemIndex];
 
-                if((itemsFilesOfBrand.length-1) == mActiveItemIdIndex){
+                if((itemsFilesOfBrand.length-1) == mActiveItemIndex){
 
                     NextBTN.disabled = true;
                     PreviousBTN.disabled = false;
 
-                }else if(mActiveItemIdIndex == 0){
+                }else if(mActiveItemIndex == 0){
 
                     NextBTN.disabled = false;
                     PreviousBTN.disabled = true;
@@ -320,6 +316,28 @@ rootController.controller("MyCntrl", ['$scope', '$http', '$location', '$window',
                 //alert(activeImageURL);
             }
 
+
+            var horizontalRow = document.getElementById("horizontalRow");
+
+            for (var t=0 ; t< horizontalRow.children.length ; t++){
+
+                if(horizontalRow.children[t].value == brandIdStr){
+
+                    horizontalRow.children[t].className = "success-active horizontalItem";
+                    horizontalRow.children[t].focus();
+
+                }else {
+
+                    if(mVsualAdsArray[t]["ISSELECTED"]){
+
+                        horizontalRow.children[t].className = "success-selected horizontalItem";
+
+                    }else{
+                        
+                        horizontalRow.children[t].className = "success-non-selected horizontalItem";
+                    }
+                }
+            }
         }
         
     }
@@ -331,16 +349,14 @@ rootController.controller("MyCntrl", ['$scope', '$http', '$location', '$window',
     }
 
  function switchBrand(brandId) {
-    mActiveItemIdIndex = 0;
-    mActiveItemIdSRN = mActiveItemIdIndex;
-    // mActiveItemIdSRN = itemsSRNOfBrand[mActiveItemIdIndex];
+    mActiveItemIndex = 0;
     mMeetingDbRfrence.set(
          {    DCR_ID: mMeetingDbSnapShot["DCR_ID"], 
               DR_ID: mMeetingDbSnapShot["DR_ID"], 
               PA_ID: mMeetingDbSnapShot["PA_ID"],
               MANAGERES_ID : mMeetingDbSnapShot["MANAGERES_ID"],
-              ACTIVE_ITEM_SRN : mActiveItemIdSRN, 
-              ACTIVE_ITEM_ID : (parseInt(mActiveItemId)-1),
+              ACTIVE_ITEM_SRN : mActiveItemIndex+"", 
+              ACTIVE_ITEM_ID : mMeetingDbSnapShot["ACTIVE_ITEM_ID"],
               ACTIVE_BRAND_ID : brandId,
               //MEETING_DATE: mMeetingDbSnapShot["MEETING_DATE"],
               MEETING_TIME: mMeetingDbSnapShot["MEETING_TIME"]
@@ -349,19 +365,19 @@ rootController.controller("MyCntrl", ['$scope', '$http', '$location', '$window',
  }
   
 $scope.goPrevious = function () {
-   if(mActiveItemIdIndex==0){
+   if(mActiveItemIndex==0){
        return;
    }
-   mActiveItemIdIndex =  (parseInt(mActiveItemIdIndex)-1);
+   mActiveItemIndex =  (parseInt(mActiveItemIndex)-1);
 
    mMeetingDbRfrence.set(
         {    DCR_ID: mMeetingDbSnapShot["DCR_ID"], 
              DR_ID: mMeetingDbSnapShot["DR_ID"], 
              PA_ID: mMeetingDbSnapShot["PA_ID"],
              MANAGERES_ID : mMeetingDbSnapShot["MANAGERES_ID"],
-             //ACTIVE_ITEM_SRN : itemsSRNOfBrand[mActiveItemIdIndex],
-             ACTIVE_ITEM_SRN : ""+mActiveItemIdIndex,
-             ACTIVE_ITEM_ID : itemsIdsOfBrand[mActiveItemIdIndex],
+             //ACTIVE_ITEM_SRN : itemsSRNOfBrand[mActiveItemIndex],
+             ACTIVE_ITEM_SRN : ""+mActiveItemIndex,
+             ACTIVE_ITEM_ID : mMeetingDbSnapShot["ACTIVE_ITEM_ID"],
              ACTIVE_BRAND_ID : mMeetingDbSnapShot["ACTIVE_BRAND_ID"],
              //MEETING_DATE: mMeetingDbSnapShot["MEETING_DATE"],
              MEETING_TIME: mMeetingDbSnapShot["MEETING_TIME"]
@@ -372,9 +388,9 @@ $scope.goPrevious = function () {
 
 $scope.goNext = function () {
     
-    mActiveItemIdIndex =  (parseInt(mActiveItemIdIndex)+1);
+    mActiveItemIndex =  (parseInt(mActiveItemIndex)+1);
 
-    if(mActiveItemIdIndex >= itemsIdsOfBrand.length){
+    if(mActiveItemIndex >= itemsFilesOfBrand.length){
         return;
     }
 
@@ -383,9 +399,9 @@ $scope.goNext = function () {
               DR_ID: mMeetingDbSnapShot["DR_ID"], 
               PA_ID: mMeetingDbSnapShot["PA_ID"],
               MANAGERES_ID : mMeetingDbSnapShot["MANAGERES_ID"],
-              //ACTIVE_ITEM_SRN : itemsSRNOfBrand[mActiveItemIdIndex],
-              ACTIVE_ITEM_SRN : ""+mActiveItemIdIndex,
-              ACTIVE_ITEM_ID : itemsIdsOfBrand[mActiveItemIdIndex],
+              //ACTIVE_ITEM_SRN : itemsSRNOfBrand[mActiveItemIndex],
+              ACTIVE_ITEM_SRN : ""+mActiveItemIndex,
+              ACTIVE_ITEM_ID : mMeetingDbSnapShot["ACTIVE_ITEM_ID"],
               ACTIVE_BRAND_ID : mMeetingDbSnapShot["ACTIVE_BRAND_ID"],
              // MEETING_DATE: mMeetingDbSnapShot["MEETING_DATE"],
               MEETING_TIME: mMeetingDbSnapShot["MEETING_TIME"]
